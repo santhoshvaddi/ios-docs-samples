4612dd2847a5dd1f5b96e43d40efcc28909fbbcf
Santhosh Vaddi <santhoshvaddi@google.com>
2019-04-18 17:35:07 -0700

    Add missing file `TokenService.swift`.

--- speech-to-speech/SpeechToSpeech/SpeechToSpeech/Services/TokenService.swift
+++ speech-to-speech/SpeechToSpeech/SpeechToSpeech/Services/TokenService.swift
@@ -1,36 +1,99 @@
 //
-//  SpeechToSpeechService.swift
-//  Speech
+// Copyright 2019 Google Inc. All Rights Reserved.
 //
-//  Created by Santhosh Kumar Vaddi on 4/11/19.
-//  Copyright © 2019 Google. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //
 
 import Foundation
 import googleapis
 import AuthLibrary
+import Firebase
 
 class TokenService {
-    
-    static let shared = TokenService()
-    var token : String?
-    
-    func fetchToken(_ completion:@escaping (ServiceError?)->()) {
-        
-        let credentialsURL = Bundle.main.url(forResource: "credentials", withExtension: "json")!
-        let scopes = ["https://www.googleapis.com/auth/cloud-platform"]
-        let provider = ServiceAccountTokenProvider(credentialsURL:credentialsURL, scopes:scopes)
-        if let provider = provider {
-            try! provider.withToken({ (token, error) in
-                if let token = token {
-                    self.token = token.AccessToken
-                    completion(nil)
-                } else {
-                    completion(.tokenNotAvailable)
-                }
-            })
+
+  static let shared = TokenService()
+  var token : String?
+
+  func fetchToken(_ completion:@escaping (ServiceError?)->()) {
+
+    let credentialsURL = Bundle.main.url(forResource: "credentials", withExtension: "json")!
+    let scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+    let provider = ServiceAccountTokenProvider(credentialsURL:credentialsURL, scopes:scopes)
+    if let provider = provider {
+      try! provider.withToken({ (token, error) in
+        if let token = token {
+          self.token = token.AccessToken
+          completion(nil)
         } else {
-            completion(.invalidCredentials)
+          completion(.tokenNotAvailable)
         }
+      })
+    } else {
+      completion(.invalidCredentials)
     }
+  }
+
+  //This func retrieves tokens from index.js
+  public func retrieveAccessToken(completionHandler: @escaping (String?, Error?) -> Void) {
+    Functions.functions().httpsCallable(ApplicationConstants.getTokenAPI).call { (result, error) in
+      if error != nil {
+        completionHandler(nil, error)
+        return
+      }
+      guard let res: HTTPSCallableResult = result else {
+        completionHandler(nil, "Result found nil" as? Error)
+        return
+      }
+      guard let tokenData = res.data as? [String: Any] else {return}
+      UserDefaults.standard.set(tokenData, forKey: ApplicationConstants.token)
+      if let accessToken = tokenData[ApplicationConstants.accessToken] as? String, !accessToken.isEmpty {
+        completionHandler(accessToken, nil)
+      }
+    }
+  }
+
+  //This function compares token expiry date with current date
+  //Returns bool value True if the token is expired else false
+  static func isExpired() -> Bool {
+    guard let token = UserDefaults.standard.value(forKey: ApplicationConstants.token) as? [String: String],
+      let expDate = token[ApplicationConstants.expireTime] else{
+        return true
+    }
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+    guard let expiryDate = dateFormatter.date(from: expDate) else {
+      return true
+    }
+    return (Date() > expiryDate)
+  }
+
+  //Return token from user defaults if token is there and not expired.
+  //Request for new token if token is expired or not there in user defaults.
+  //Return the newly generated token.
+  static func getToken(completionHandler: @escaping (String)->Void) {
+    if isExpired() {
+      guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+        return completionHandler("")
+      }
+      appDelegate.retrieveAccessToken { (result) in
+        completionHandler(result)
+      }
+    } else {
+      guard let token = UserDefaults.standard.value(forKey: ApplicationConstants.token) as? [String: String],
+        let accessToken = token[ApplicationConstants.accessToken] else {
+          return completionHandler("")
+      }
+      return completionHandler(accessToken)
+    }
+  }
 }
